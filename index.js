1d23b4abefc535d1941ba34faea2632c73352c98
Aquariuslt <superaquariuslt@gmail.com>
2017-09-05 00:05:19 +0800

    [Config] Pre-setup project lint config.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 /* Created By Aquariuslt at 2017-09-03 */
+import marked from 'marked';
+
+let compiler = {
+  compile: function(source) {
+    return marked(source);
+  }
+};
+
+export {
+  compiler
+};
